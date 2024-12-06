aba8fe2e364576adbc8054b693c5cfba6c0b6f0e
jerkytoad <hermanr@chalmers.se>
2024-12-06 11:04:36 +0100

    Create sustainability_check.py

---
+++ Basic_Simulation/sustainability_check.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 10:26:42 2024
+
+@author: herman
+"""
+
+import numpy as np
+
+def SustainabilityCheck(age_list, mean_age):
+    
+    Ni, Nj = np.where(age_list <= mean_age)
+    remaining_trees = len(Ni)
+    Mi, Mj = np.where(age_list == 0)
+    empty_spots = len(Mi)
+    total_number_of_trees = np.size(age_list) - empty_spots
+    percentage = remaining_trees/total_number_of_trees
+    
+    
+    return percentage
+
+#%%
+
+# forest_size = 64
+
+# age_list = np.random.randint(0,100,[forest_size, forest_size])
+# mean_age = 50
+
+# remainingTreesPercent = SustainabilityCheck(age_list, mean_age)
+
+# print(remainingTreesPercent)
