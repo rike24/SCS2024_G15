1003721fd92421681056373ba181ecee5c2c18c0
rike24 <152967785+rike24@users.noreply.github.com>
2024-12-11 14:39:36 +0100

    Update plots.py
    Insert new plot function for batch run that overlays plots

--- Basic_Simulation/plots.py
+++ Basic_Simulation/plots.py
@@ -31,3 +31,32 @@ def plotForestData(forest_data, x_label, y_label, vline_x = None, hline_y = None
             plt.legend(["Forest " + str(i) for i in range(forest_amount)])
     plt.show()
 
+def plotForestBatchData(forest_data, x_label, y_label, vline_x = None, hline_y = None, line_name = None):
+    forest_amount = len(forest_data)
+    for i in range(forest_amount):
+        plt.plot(range(len(forest_data[i])), forest_data[i])
+    
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    
+    if vline_x != None:
+        if (type(vline_x) == list):
+            for i in range(len(vline_x)):
+                plt.axvline(vline_x[i], linestyle="dashed")
+        else:
+            plt.axvline(vline_x, linestyle="dashed")
+    
+    if hline_y != None:
+        if (type(hline_y) == list):
+            for i in range(len(hline_y)):
+                plt.axvline(hline_y[i], linestyle="dashed")
+        else:
+            plt.axvline(hline_y, linestyle="dashed")
+    if (line_name != None):
+        line_name_list = [line_name] if (type(line_name) != list) else line_name
+        if (forest_amount > 1):
+            plt.legend(["Forest " + str(i) for i in range(forest_amount)] + line_name_list)
+    else:
+        if (forest_amount > 1):
+            plt.legend(["Forest " + str(i) for i in range(forest_amount)])
+    plt.show()
